abf3af593e0f22fc405f745d2b79a55424e431d9
kisu-park <kisu.park@navercorp.com>
2020-09-19 23:41:19 +0900

    fix bugs
    - Add TLSTextPlain
    - Add SignatureAlgorithms

--- TLSProtocol/TLSProtocol/TLSProtocol.swift
+++ TLSProtocol/TLSProtocol/TLSProtocol.swift
@@ -18,6 +18,7 @@ public struct ProtocolVersion {
         self.minor = minor
     }
     
+    public static let TLS_1_0 = ProtocolVersion(major: 3, minor: 1)
     public static let TLS_1_2 = ProtocolVersion(major: 3, minor: 3)
 }
 
@@ -82,6 +83,41 @@ public struct CompressionMethod {
     public static let NULL = CompressionMethod(rawValue: 0)
 }
 
+public enum ContentType: Int {
+    case change_cipher_spec = 20
+    case alert = 21
+    case handshake = 22
+    case application_data = 23
+}
+
+public struct TLSPlaintext {
+    public let type: ContentType
+    public let version: ProtocolVersion
+    public let fragment: [UInt8]
+    
+    public init(type: ContentType, version: ProtocolVersion, fragment: [UInt8]) {
+        self.type = type
+        self.version = version
+        self.fragment = fragment
+    }
+    
+    public var bytes: [UInt8] {
+        var buffer: [UInt8] = []
+        buffer.append(UInt8(self.type.rawValue))
+        
+        buffer.append(self.version.major)
+        buffer.append(self.version.minor)
+        
+        var lengthOfFragment: UInt16 = UInt16(self.fragment.count).bigEndian
+        withUnsafeBytes(of: &lengthOfFragment) { (lengthOfFragment) -> Void in
+            buffer.append(contentsOf: lengthOfFragment.bindMemory(to: UInt8.self))
+        }
+        buffer.append(contentsOf: self.fragment)
+        return buffer
+    }
+}
+
+
 public enum HandshakeType: Int {
     case hello_request = 0
     case client_hello = 1
@@ -107,12 +143,14 @@ public struct Handshake {
         var buffer: [UInt8] = []
         buffer.append(UInt8(self.type.rawValue))
         let body = clientHello.bytes
-        var countOfBody = body.count
-        withUnsafeBytes(of: &countOfBody) { (count) -> Void in
-            buffer.append(count[2])
-            buffer.append(count[1])
-            buffer.append(count[0])
+        var lengthOfBody = UInt32(body.count).bigEndian
+        withUnsafeBytes(of: &lengthOfBody) { (_lengthOfBody) -> Void in
+            let lengthOfBody = _lengthOfBody.bindMemory(to: UInt8.self)
+            buffer.append(lengthOfBody[1])
+            buffer.append(lengthOfBody[2])
+            buffer.append(lengthOfBody[3])
         }
+        
         buffer.append(contentsOf: body)
         return buffer
     }
@@ -138,21 +176,20 @@ public struct ClientHello {
     public var random: Random
     public var sessionID: SessionID?
     public var cipherSuites: [CipherSuite] = []
-    public var compressionMethod: CompressionMethod
+    public var compressionMethods: [CompressionMethod] = []
     public var extensions: [Extension] = []
     
-    public init(_ clientVersion: ProtocolVersion, _ random: Random, _ sessionID: SessionID? = nil, _ cipherSuites: [CipherSuite], _ compressionMethod: CompressionMethod, _ extensions: [Extension]) {
+    public init(_ clientVersion: ProtocolVersion, _ random: Random, _ sessionID: SessionID? = nil, _ cipherSuites: [CipherSuite], _ compressionMethods: [CompressionMethod], _ extensions: [Extension]) {
         self.clientVersion = clientVersion
         self.random = random
         self.sessionID = sessionID
         self.cipherSuites = cipherSuites
-        self.compressionMethod = compressionMethod
+        self.compressionMethods = compressionMethods
         self.extensions = extensions
     }
     
     public var bytes: [UInt8] {
         var buffer: [UInt8] = []
-        buffer.append(22)
         buffer.append(self.clientVersion.major)
         buffer.append(self.clientVersion.minor)
         var random = self.random
@@ -163,6 +200,42 @@ public struct ClientHello {
             buffer.append(random[0])
         }
         buffer.append(contentsOf: random.random_bytes)
+        
+        if let sessionID = self.sessionID?.data(using: .ascii) {
+            buffer.append(UInt8(sessionID.count))
+            sessionID.withUnsafeBytes { (sessionID) -> Void in
+                buffer.append(contentsOf: sessionID)
+            }
+        } else {
+            buffer.append(0)
+        }
+        
+        var lengthOfCipherSuites: UInt16 = UInt16(self.cipherSuites.count * MemoryLayout<CipherSuite>.size).bigEndian
+        withUnsafeBytes(of: &lengthOfCipherSuites) { (lengthOfCipherSuites) -> Void in
+            buffer.append(contentsOf: lengthOfCipherSuites)
+        }
+        self.cipherSuites.forEach {
+            buffer.append($0.a)
+            buffer.append($0.b)
+        }
+        
+        let lengthOfCompressionMethods: UInt8 = UInt8(self.compressionMethods.count * MemoryLayout<CompressionMethod>.size)
+        buffer.append(lengthOfCompressionMethods)
+        
+        self.compressionMethods.forEach { (method) in
+            buffer.append(method.rawValue)
+        }
+        
+        var extensionBytes: [UInt8] = []
+        self.extensions.forEach { (extension) in
+            extensionBytes.append(contentsOf: `extension`.bytes)
+        }
+        var lengthOfExtensions: UInt16 = UInt16(extensionBytes.count).bigEndian
+        withUnsafeBytes(of: &lengthOfExtensions) { (lengthOfExtensions) -> Void in
+            buffer.append(contentsOf: lengthOfExtensions)
+        }
+        buffer.append(contentsOf: extensionBytes)
+
         return buffer
     }
 };
@@ -188,45 +261,44 @@ public struct ServerHello {
     public var random: Random
     public var sessionID: SessionID?
     public var cipherSuites: [CipherSuite] = []
-    public var compressionMethod: CompressionMethod
+    public var compressionMethods: [CompressionMethod]
     public var extensions: [Extension] = []
     
-    public init(_ serverVersion: ProtocolVersion, _ random: Random, _ sessionID: SessionID? = nil, _ cipherSuites: [CipherSuite], _ compressionMethod: CompressionMethod, _ extensions: [Extension]) {
+    public init(_ serverVersion: ProtocolVersion, _ random: Random, _ sessionID: SessionID? = nil, _ cipherSuites: [CipherSuite], _ compressionMethods: [CompressionMethod], _ extensions: [Extension]) {
         self.serverVersion = serverVersion
         self.random = random
         self.sessionID = sessionID
         self.cipherSuites = cipherSuites
-        self.compressionMethod = compressionMethod
+        self.compressionMethods = compressionMethods
         self.extensions = extensions
     }
 };
 
 
 
-public struct Extension {
+public protocol Extension {
+    var type: ExtensionType { get }
+    var bytes: [UInt8] { get }
+}
 
-    public enum ExtensionType: Int {
-        case serverName = 0
-        case maxFragmentLength = 1
-        case clientCertificateURL = 2
-        case trustedCaKeys = 3
-        case truncatedHMAC = 4
-        case statusRequest = 5
-        case supportedGroups = 10
-        case ecPointFormats = 11
-        case signatureAlgorithms = 13
-        case applicationLayerProtocolNegotiation = 16
-        case signedCertificateTimestamp = 18
-        case padding = 21
-        case extendedMasterSecret = 23
-        case supportedVersions = 43
-        case pskKeyExchangeModes = 45
-        case keyShare = 51
-        case renegotiationInfo = 65281
-    }
-    
-    public let type: ExtensionType
-    public let data: Data // max length: 2^16-1 = 65535
+public enum ExtensionType: Int {
+    case serverName = 0
+    case maxFragmentLength = 1
+    case clientCertificateURL = 2
+    case trustedCaKeys = 3
+    case truncatedHMAC = 4
+    case statusRequest = 5
+    case supportedGroups = 10
+    case ecPointFormats = 11
+    case signatureAlgorithms = 13
+    case applicationLayerProtocolNegotiation = 16
+    case signedCertificateTimestamp = 18
+    case padding = 21
+    case extendedMasterSecret = 23
+    case supportedVersions = 43
+    case pskKeyExchangeModes = 45
+    case keyShare = 51
+    case renegotiationInfo = 65281
 }
 
 public enum HashAlgorithm: Int {
@@ -249,4 +321,48 @@ public enum SignatureAlgorithm: Int {
 public struct SignatureAndHashAlgorithm {
     let hash: HashAlgorithm
     let signature: SignatureAlgorithm
+    
+    public init(_ hash: HashAlgorithm, _ signature: SignatureAlgorithm) {
+        self.hash = hash
+        self.signature = signature
+    }
+}
+
+public struct SignatureAlgorithms: Extension {
+    public let type: ExtensionType = .signatureAlgorithms
+    public let signatureAndHashAlgorithms: [SignatureAndHashAlgorithm]
+    
+    public init(_ signatureAndHashAlgorithms: [SignatureAndHashAlgorithm]) {
+        self.signatureAndHashAlgorithms = signatureAndHashAlgorithms
+    }
+    
+    public var bytes: [UInt8] {
+        var buffer: [UInt8] = []
+        var type = UInt16(self.type.rawValue).bigEndian
+        withUnsafeBytes(of: &type) { (type) -> Void in
+            buffer.append(contentsOf: type.bindMemory(to: UInt8.self))
+        }
+        
+        var algorithms: [UInt8] = []
+        
+        self.signatureAndHashAlgorithms.forEach {
+            algorithms.append(UInt8($0.hash.rawValue))
+            algorithms.append(UInt8($0.signature.rawValue))
+        }
+        
+        var body: [UInt8] = []
+        var lengthOfAlgorithms: UInt16 = UInt16(algorithms.count).bigEndian
+        withUnsafeBytes(of: &lengthOfAlgorithms) { (lengthOfAlgorithms) -> Void in
+            body.append(contentsOf: lengthOfAlgorithms.bindMemory(to: UInt8.self))
+        }
+        body.append(contentsOf: algorithms)
+        
+        var length: UInt16 = UInt16(body.count).bigEndian
+        withUnsafeBytes(of: &length) { (length) -> Void in
+            buffer.append(contentsOf: length.bindMemory(to: UInt8.self))
+        }
+        
+        buffer.append(contentsOf: body)
+        return buffer
+    }
 }
