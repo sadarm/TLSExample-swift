fd210ccb94d328c826cd5387872c80bf5eca7d0b
kisu-park <kisu.park@navercorp.com>
2020-09-19 15:07:27 +0900

    Add Errors

---
+++ TLSProtocol/TLSProtocol/Errors.swift
@@ -0,0 +1,15 @@
+//
+//  Errors.swift
+//  TLSProtocol
+//
+//  Created by kisu Park on 2020/09/19.
+//  Copyright © 2020 sadarm. All rights reserved.
+//
+
+import Foundation
+
+enum TLSProtocolError: Error {
+    case outOfRange
+    case invalidEncoding
+}
+
